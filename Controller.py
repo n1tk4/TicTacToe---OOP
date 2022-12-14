eea4fc6ef3517693e70acfe8fdcdf5bcd657db71
n1tk4 <119902234+n1tk4@users.noreply.github.com>
2022-12-14 19:24:26 +0100

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -1,17 +1,15 @@
 from Model import *
 from View import *
 import json
+from functools import lru_cache
 
 
 class Controller:
     """Processes user input and applies it to the view and/or model"""
-
     def __init__(self):
         self.model = Model()
         self.view = View(Model())
         self.call_function_times = 0
-
-
 
     def get_move(self):
         self.chosen_position = None
@@ -34,20 +32,17 @@ class Controller:
         return self.chosen_position
 
     def is_empty(self, board, chosen_position):
-        while board[chosen_position] != " ":
+        while board[chosen_position] != ' ':
             self.view.print_is_taken()
             chosen_position = self.get_move()
             return True
         return False
 
-
     def make_move(self, board, chosen_position, player):
-        while board[chosen_position] != " ":
-            print(f"Can't make move {chosen_position}, square already taken!")
+        while board[chosen_position] != ' ':
+            print(f"Can't make move {chosen_position}, square already taken!") # realize in print view
             chosen_position = self.get_move()
         board[chosen_position] = player
-
-
 
     def player(self):
         if self.model.player == 'O':
@@ -56,7 +51,6 @@ class Controller:
             self.model.player = 'O'
             # print O is turn
         return self.model.player
-
 
     def computer(self, board):
         bestScore = -1000
@@ -82,6 +76,7 @@ class Controller:
         board = self.model.board
         board[computer] = 'O'
 
+    @lru_cache()
     def minimax(self, is_maximizing):
         print(f"position in calculation {self.model.board}")
         self.call_function_times = self.call_function_times + 1
@@ -93,6 +88,7 @@ class Controller:
             return -1
         elif self.model.is_draw():
             return 0
+        self.model.winner = ' '
         if is_maximizing:
             bestScore = -99
             for key in self.model.board.keys():
@@ -112,7 +108,7 @@ class Controller:
                     self.model.board[key] = ' '
                     if (score < bestScore):
                         bestScore = score
-            self.mcall = 0
+            self.call_function_times = 0
             return bestScore
 
     def save_game(self):
@@ -132,33 +128,47 @@ class Controller:
             board = {int(k):str(v) for k,v in board.items()}
         return board
 
-
-    def player_mode(self):
-        inp = input("Do you want load the previous game? y/n: ")
+    def ask_load(self):
+        inp = input(self.view.want_previous_game())
         if inp == 'y':
             self.model.board = self.load_game()
+
+    def player_mode(self):
+        self.ask_load()
         while True:
             self.view.print_board(self.model.board)
             self.save_game()
             if self.model.get_winner() != ' ':
-                print(self.model.get_winner())
+                self.view.print_winner()
                 break
             elif self.model.is_draw():
                 self.view.print_draw()
                 break
             self.make_move(self.model.board, self.get_move(), self.player())
 
-
     def ai_mode(self):
+        self.ask_load()
         counter = 0
         while True:
             self.view.print_board(self.model.board)
             if self.model.get_winner() != ' ':
-                print(f"WInner is {self.model.get_winner()}")
+                self.view.print_winner()
                 break
             if counter % 2 == 0:
                 self.make_move(self.model.board, self.get_move(), self.player())
-                print(self.model.get_winner())
+                self.view.print_winner()
             else:
                 self.make_move(self.model.board, self.computer(self.model.board), self.player())
+                self.model.winner=' '
             counter += 1
+
+    def which_mode(self):
+        inp = input(self.view.choose_mode())
+        if inp == 'a':
+            self.ai_mode()
+        else:
+            self.player_mode()
+
+    def play(self):
+        self.view.greet()
+        self.which_mode()
